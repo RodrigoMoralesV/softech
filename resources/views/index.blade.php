efa84c97404e0673ba26d4ddec1a04691fe8f4e3
richard RR <richard520ruiz@gmail.com>
2024-05-01 15:33:39 -0500

    modified:   app/Http/Controllers/CarritoController.php 	modified:   public/css/style.css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -103,7 +103,7 @@
 								<div class="dropdown">
 									<a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
 										<i class="fa fa-shopping-cart"></i>
-										<span>Your Cart</span>
+										<span>Carrito</span>
 										<div class="qty">3</div>
 									</a>
 									<div class="cart-dropdown">
@@ -113,8 +113,9 @@
 													<img src="./img/product01.png" alt="">
 												</div>
 												<div class="product-body">
-													<h3 class="product-name"><a href="#">product name goes here</a></h3>
-													<h4 class="product-price"><span class="qty">1x</span>$980.00</h4>
+													<h3 class="product-name"><a href="#">Portatil php </a></h3>
+													<h4>Precio </span>$980.00</h4>	
+													<h4 class="product-price">Cantidad <span class="qty">1x
 												</div>
 												<button class="delete"><i class="fa fa-close"></i></button>
 											</div>
@@ -124,19 +125,19 @@
 													<img src="./img/product02.png" alt="">
 												</div>
 												<div class="product-body">
-													<h3 class="product-name"><a href="#">product name goes here</a></h3>
-													<h4 class="product-price"><span class="qty">3x</span>$980.00</h4>
+													<h3 class="product-name"><a href="#">Audifonos Apple</a></h3>
+													<h4>Precio </span>$980.00</h4>
+													<h4 class="product-price">Cantidad <span class="qty">3x
 												</div>
 												<button class="delete"><i class="fa fa-close"></i></button>
 											</div>
 										</div>
 										<div class="cart-summary">
-											<small>3 Item(s) selected</small>
+											<small>Productos Seleccionados</small>
 											<h5>SUBTOTAL: $2940.00</h5>
 										</div>
 										<div class="cart-btns">
-											<a href="#">View Cart</a>
-											<a href="#">Checkout  <i class="fa fa-arrow-circle-right"></i></a>
+										<button type='button'class="btn-btn light" style="border-radius: 20px;"><a href="#">Continuar</a></button>
 										</div>
 									</div>
 								</div>
